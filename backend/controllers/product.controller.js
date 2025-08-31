20abbe4434aa32b920738d3d5394a36d8d84835a
ioan-murchello <mihalkurz@gmail.com>
2025-08-31 10:21:46 +0200

    added description field in createProduct

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -16,7 +16,7 @@ export const getProducts = async (req, res) => {
 export const createProduct = async (req, res) => {
 	const product = req.body; // user will send this data
 
-	if (!product.name || !product.price || !product.image) {
+	if (!product.name || !product.price || !product.image || !product.description) {
 		return res.status(400).json({ success: false, message: "Please provide all fields" });
 	}
 
@@ -30,6 +30,24 @@ export const createProduct = async (req, res) => {
 		res.status(500).json({ success: false, message: "Server Error" });
 	}
 };
+
+// export const createProduct = async (req, res) => {
+// 	const product = req.body; // user will send this data
+
+// 	if (!product.name || !product.price || !product.image) {
+// 		return res.status(400).json({ success: false, message: "Please provide all fields" });
+// 	}
+
+// 	const newProduct = new Product(product);
+
+// 	try {
+// 		await newProduct.save();
+// 		res.status(201).json({ success: true, data: newProduct });
+// 	} catch (error) {
+// 		console.error("Error in Create product:", error.message);
+// 		res.status(500).json({ success: false, message: "Server Error" });
+// 	}
+// };
 
 export const updateProduct = async (req, res) => {
 	const { id } = req.params;
